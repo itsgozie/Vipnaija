a75f590d76ab1f5ce2019916e09cacad41ba9f0d
Gozie <87874051+itsgozie@users.noreply.github.com>
2022-07-20 22:46:02 +0100

    July 20th

--- main.js
+++ main.js
@@ -1,3 +1,3 @@
 let userName;
-userName = prompt("What's your name?");
-alert(`Hello ${userName} welcome to VipNaija`);
+// userName = prompt("What's your name?");
+// alert(`Hello ${userName} welcome to VipNaija`);
